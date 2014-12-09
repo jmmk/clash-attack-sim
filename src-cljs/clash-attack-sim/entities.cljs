28bff9edfdb79f94f7aeb86362ac0e12ca37a0d2
Michael McLellan <jmikem825@gmail.com>
2014-12-08 22:47:31 -0500

    Add helper methods to reduce duplication

--- src-cljs/clash-attack-sim/entities.cljs
+++ src-cljs/clash-attack-sim/entities.cljs
@@ -7,34 +7,26 @@
 (def ^:const barbarian-speed 0.25)
 
 (defn barbarian [x y]
-  (let [sprite (js/PIXI.Sprite. (helper/load-texture "images/barbarian-neutral-down.png"))
+  (let [neutral (js/PIXI.Sprite. (helper/load-texture "images/barbarian-neutral-down.png"))
         right (js/PIXI.Sprite. (helper/load-texture "images/barbarian-run-right-down.png"))
         left (js/PIXI.Sprite. (helper/load-texture "images/barbarian-run-left-down.png"))
-        sprite-list {:neutral sprite :right right :left left}
+        sprite-list {:neutral neutral :right right :left left}
         animation-seq [:neutral :left :neutral :right]]
-    (sprite/set-pos! sprite "x" x)
-    (sprite/set-pos! sprite "y" y)
-    (sprite/set-dimension! sprite "height" 1)
-    (sprite/set-dimension! sprite "width" 1)
-
-    (sprite/set-dimension! right "height" 1)
-    (sprite/set-dimension! right "width" 1)
-
-    (sprite/set-dimension! left "height" 1)
-    (sprite/set-dimension! left "width" 1)
+    (sprite/set-pos! neutral x y)
+    (sprite/resize! neutral 1 1)
+    (sprite/resize! right 1 1)
+    (sprite/resize! left 1 1)
 
     (ecs/entity (component/position x y)
                 (component/movement barbarian-speed)
                 (component/animation sprite-list animation-seq 0)
-                (component/renderable sprite)
+                (component/renderable neutral)
                 (component/attacker nil))))
 
 (defn town-hall [x y]
   (let [sprite (js/PIXI.Sprite. (helper/load-texture "images/town-hall.png"))]
-    (sprite/set-pos! sprite "x" x)
-    (sprite/set-pos! sprite "y" y)
-    (sprite/set-dimension! sprite "height" 4)
-    (sprite/set-dimension! sprite "width" 4)
+    (sprite/set-pos! sprite x y)
+    (sprite/resize! sprite 4 4)
 
     (ecs/entity (component/position x y)
             (component/renderable sprite)
--- src-cljs/clash-attack-sim/sprite.cljs
+++ src-cljs/clash-attack-sim/sprite.cljs
@@ -1,12 +1,17 @@
 (ns clash-attack-sim.sprite
   (:require [clash-attack-sim.helper :as helper]))
 
-(defn set-pos! [sprite axis value]
+(defn set-pos! [sprite x y]
   (let [position (.-position sprite)]
-    (helper/set-property! position axis value))
+    (helper/set-property! position "x" x)
+    (helper/set-property! position "y" y))
   sprite)
 
 (defn set-dimension! [sprite dimension tiles]
   (let [size (helper/get-tile-size tiles)]
     (helper/set-property! sprite dimension size))
   sprite)
+
+(defn resize! [sprite width height]
+  (set-dimension! sprite "width" width)
+  (set-dimension! sprite "height" height))

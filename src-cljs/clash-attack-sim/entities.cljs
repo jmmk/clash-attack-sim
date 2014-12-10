f18eb41b0a42eafca536996ae74619d3ff263407
Michael McLellan <jmikem825@gmail.com>
2014-12-10 06:23:19 -0500

    Anchor sprites at (0.5, 0.5) to operate based on their center

--- src-cljs/clash-attack-sim/entities.cljs
+++ src-cljs/clash-attack-sim/entities.cljs
@@ -6,12 +6,17 @@
 
 (def ^:const barbarian-speed 0.25)
 
+(def center-anchor (js/PIXI.Point. 0.5 0.5))
+
 (defn barbarian [x y]
   (let [neutral (sprite/from-frame "barbarian-neutral-down.png")
         right (sprite/from-frame "barbarian-run-right-down.png")
         left (sprite/from-frame "barbarian-run-left-down.png")
         sprite-list {:neutral neutral :right right :left left}
         animation-seq [:neutral :left :neutral :right]]
+    (helper/set-property! neutral "anchor" center-anchor)
+    (helper/set-property! right "anchor" center-anchor)
+    (helper/set-property! left "anchor" center-anchor)
     (sprite/set-pos! neutral x y)
     (sprite/resize! neutral 1 1)
     (sprite/resize! right 1 1)
@@ -27,6 +32,7 @@
   (let [sprite (sprite/from-image "images/town-hall.png")]
     (sprite/set-pos! sprite x y)
     (sprite/resize! sprite 4 4)
+    (helper/set-property! sprite "anchor" center-anchor)
 
     (ecs/entity (component/position x y)
             (component/renderable sprite)

c0d561b7c091e5ea4c389596833d5c76e737eae1
Michael McLellan <jmikem825@gmail.com>
2014-12-23 16:35:41 -0600

    Add defsystem macro

--- src/clash_attack_sim/macro.clj
+++ src/clash_attack_sim/macro.clj
@@ -19,3 +19,41 @@
     `(do (defrecord ~capital-name ~(into '[name] fields))
           (defn ~lowercase-name ~fields
                 (~record-constructor ~keyword-name ~@fields)))))
+
+(defmacro defsystem [name args & attributes]
+  "Define a system using the syntax:
+  (defsystem attack [world]
+    :entities {attacking [:attacker :attacking]
+                alive [:attackable :alive]}
+    :frame-period 5
+    :fn do-attacks)
+
+  Generates:
+  (defn attack-system [world]
+    (if (= (mod (:frame-count world) 5) 0)
+      (let [attackers (ecs/get-entities-with-components :attacker :attacking)
+            alive (ecs/get-entities-with-components :attackable :alive)]
+        (if-not (and (empty? attackers)
+                     (empty? alive))
+          (do-attacks world attackers alive)
+          world))
+      world))"
+
+  (let [function-name (symbol (str name "-system"))
+        attr-map (apply hash-map attributes)
+        entities (:entities attr-map)
+        frame-period (:frame-period attr-map)
+        function (:fn attr-map)]
+    (list 'defn function-name args
+          (list 'if
+                (list '=
+                      (list 'mod (list ':frame-count 'world) frame-period)
+                      0)
+                (list 'let (into [] (apply concat (seq (into {} (for [[k v] entities
+                                                         :let [new-v '(apply ecs/get-entities-with-components (into '[world] v))]]
+                                                         [k new-v])))))
+                                 (list 'if-not (into (for [k (keys entities)]
+                                                            (list 'empty? k)) '(and))
+                                       (list 'apply function (into '[world] (keys entities)))
+                                       'world))
+                'world))))

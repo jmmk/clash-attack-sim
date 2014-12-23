5eb91e07d9f67a79563026313275ec7a41529dc9
Michael McLellan <jmikem825@gmail.com>
2014-12-22 19:48:18 -0600

    Allow multiple clicks per frame

--- src-cljs/clash-attack-sim/input.cljs
+++ src-cljs/clash-attack-sim/input.cljs
@@ -2,19 +2,22 @@
   (:require [clash-attack-sim.ecs :as ecs]
             [clash-attack-sim.entities :as entities]))
 
-(def clicks (atom {}))
+(def clicks (atom []))
 
 (defn stage-click [data]
   (let [target (.-target data)
         point (.getLocalPosition data target)
         x (.-x point)
         y (.-y point)]
-    (reset! clicks {:x x :y y})))
+    (swap! clicks conj {:x x :y y})))
 
 (defn input-system [world]
-  (if-not (empty? @clicks)
-    (let [x (:x @clicks)
-          y (:y @clicks)]
-      (reset! clicks {})
-      (ecs/assoc-entity world (entities/barbarian x y)))
-    world))
+  (let [last-clicks @clicks]
+    (reset! clicks [])
+    (if (seq last-clicks)
+      (ecs/assoc-entities world
+                          (for [click last-clicks]
+                            (let [x (:x click)
+                                  y (:y click)]
+                              (entities/barbarian x y))))
+      world)))

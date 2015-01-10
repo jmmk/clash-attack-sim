a46c4a4b1fe1af4b27db665c406c1ff71267f1b4
Michael McLellan <jmikem825@gmail.com>
2015-01-10 08:40:52 -0500

    Clean up project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
-(defproject clash-attack-sim "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clash-attack-sim "0.1.0"
+  :description "Clash of Clans Attack Simulator"
+  :url "https://www.github.com/jmmk/clash-attack-sim"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.1"]
                  [criterium "0.4.3"]
